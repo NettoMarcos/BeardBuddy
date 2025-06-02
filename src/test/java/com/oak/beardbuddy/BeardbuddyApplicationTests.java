f9c493b840d35e885437ca32ea510951fe776b75
Marcos Netto <marcoscarvalhonetto199@gmail.com>
2025-06-02 00:16:16 -0300

    correção deploy

--- src/test/java/com/oak/beardbuddy/BeardbuddyApplicationTests.java
+++ src/test/java/com/oak/beardbuddy/BeardbuddyApplicationTests.java
@@ -2,8 +2,10 @@ package com.oak.beardbuddy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BeardbuddyApplicationTests {
 
 	@Test
